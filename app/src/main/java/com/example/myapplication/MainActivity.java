e40205faab7c9d2705e000a5b622953351d8c098
nb mu <975488243@qq.com>
2019-11-07 00:06:53 +0800

    handle exprise view

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,29 @@
 package com.example.myapplication;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import org.xmlpull.v1.XmlPullParser;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,17 +33,18 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvPianjia;
     private TextView tvRoman;
     private List<VoiceItem> voiceItemList;
-    private int voiceIndex = 0;
+    private int viewVoiceIndex = 0;
+    private int expriseVoiceIndex = 0;
     private boolean isPlay = false;
-    private static int delay = 0;
-    private static int period = 2000;  //2s
     private static final int Update_View = 0;
     private Timer mTimer = null;
     private TimerTask mTimerTask = null;
     private Handler mHandler = null;
     private MediaPlayer player = null;
-
-
+    private EditText tb_exprise_hiragana;
+    private EditText tb_exprise_katakana;
+    private EditText tb_exprise_roman;
+    private Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,34 +56,231 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("HandlerLeak")
     private void initViews()
     {
-        TabHost tabHost = (TabHost) this.findViewById(android.R.id.tabhost);
-        tabHost.setup();
-        LayoutInflater inflater = LayoutInflater.from(this);
-        inflater.inflate(R.layout.voice_view,tabHost.getTabContentView());
-        inflater.inflate(R.layout.voice_exprise,tabHost.getTabContentView());
-        tabHost.addTab(tabHost.newTabSpec("voice_view").setIndicator("学习").setContent(R.id.left));
-        tabHost.addTab(tabHost.newTabSpec("voice_exprise").setIndicator("练习").setContent(R.id.right));
-
+        initTabs();
+        voiceItemList = loadXml();
         player = new MediaPlayer();
-        final Button btnPlay = this.findViewById(R.id.btnPlay);
+        initView();
+        initExpriseView();
+    }
+
+    private void initExpriseView(){
+        final Button btnCheck = this.findViewById(R.id.btn_exprise_check);
+        final Button btnPrev = this.findViewById(R.id.btn_exprise_prev);
+        final Button btnNext = this.findViewById(R.id.btn_exprise_next);
+        final Button btnVoice = this.findViewById(R.id.btn_exprise_voice);
+
+        tb_exprise_hiragana = this.findViewById(R.id.tb_exprise_hiragana);
+        tb_exprise_katakana = this.findViewById(R.id.tb_exprise_katakana);
+        tb_exprise_roman = this.findViewById(R.id.tb_exprise_roman);
+
+        tb_exprise_hiragana.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tb_exprise_hiragana.setTextColor(Color.BLACK);
+            }
+        });
+
+        tb_exprise_katakana.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tb_exprise_katakana.setTextColor(Color.BLACK);
+            }
+        });
+
+        tb_exprise_roman.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tb_exprise_roman.setTextColor(Color.BLACK);
+            }
+        });
+
+        RadioGroup rg_exprise_mode = this.findViewById(R.id.rg_exprise_mode);
+
+        btnCheck.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                StringBuilder sb = new StringBuilder();
+                if(tb_exprise_hiragana.getText().length() == 0){
+                    sb.append("请输入平假名!\n");
+                }
+
+                if(tb_exprise_katakana.getText().length() == 0){
+                    sb.append("请输入片假名!\n");
+                }
+
+                if(tb_exprise_roman.getText().length() == 0){
+                    sb.append("请输入罗马音!\n");
+                }
+
+                if(sb.length() > 0){
+                    sb.deleteCharAt(sb.lastIndexOf("\n"));
+                    Toast toast =  Toast.makeText(MainActivity.this,sb.toString(),Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else{
+                    VoiceItem item = (VoiceItem)tb_exprise_roman.getTag();
+                    if(item !=null){
+                        if(tb_exprise_roman.isEnabled()){
+                            if(!tb_exprise_roman.getText().toString().equals(item.getRoman())){
+                                tb_exprise_roman.setTextColor(Color.RED);
+                            }
+                            else{
+                                tb_exprise_roman.setTextColor(Color.GREEN);
+                            }
+                        }
+
+                        if(tb_exprise_hiragana.isEnabled()){
+                            if(!tb_exprise_hiragana.getText().toString().equals(item.getHiragana())){
+                                tb_exprise_hiragana.setTextColor(Color.RED);
+                            }
+                            else{
+                                tb_exprise_hiragana.setTextColor(Color.GREEN);
+                            }
+                        }
+
+                        if(tb_exprise_katakana.isEnabled()){
+                            if(!tb_exprise_katakana.getText().toString().equals(item.getKatakana())){
+                                tb_exprise_katakana.setTextColor(Color.RED);
+                            }
+                            else{
+                                tb_exprise_katakana.setTextColor(Color.GREEN);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        btnPrev.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                expriseVoiceIndex=expriseVoiceIndex-1;
+                if(expriseVoiceIndex < 0){
+                    expriseVoiceIndex = voiceItemList.size();
+                }
+                setExproseViewContent(voiceItemList.get(expriseVoiceIndex),true);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                expriseVoiceIndex=expriseVoiceIndex+1;
+                if(expriseVoiceIndex > voiceItemList.size()){
+                    expriseVoiceIndex = 0;
+                }
+                setExproseViewContent(voiceItemList.get(expriseVoiceIndex),true);
+            }
+        });
+
+        btnVoice.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+               VoiceItem item =(VoiceItem)tb_exprise_roman.getTag();
+               if(item != null)
+               {
+                   playVoice(item);
+               }
+            }
+        });
+
+        rg_exprise_mode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_exprise_kana:
+                        tb_exprise_hiragana.setText("");
+                        tb_exprise_katakana.setText("");
+                        tb_exprise_hiragana.setEnabled(true);
+                        tb_exprise_katakana.setEnabled(true);
+                        tb_exprise_roman.setEnabled(false);
+                        break;
+                    case R.id.rb_exprise_roman:
+                        tb_exprise_hiragana.setEnabled(false);
+                        tb_exprise_katakana.setEnabled(false);
+                        tb_exprise_roman.setEnabled(true);
+                        break;
+                    case R.id.rb_exprise_kanaAndRoman:
+                        tb_exprise_hiragana.setText("");
+                        tb_exprise_katakana.setText("");
+                        tb_exprise_roman.setText("");
+                        tb_exprise_hiragana.setEnabled(true);
+                        tb_exprise_katakana.setEnabled(true);
+                        tb_exprise_roman.setEnabled(true);
+                        break;
+                }
+                setExproseViewContent(voiceItemList.get(expriseVoiceIndex),false);
+            }
+        });
+
+        setExproseViewContent(voiceItemList.get(expriseVoiceIndex),false);
+    }
+
+    private void setExproseViewContent(VoiceItem item, boolean playVoice) {
+        if(tb_exprise_roman.isEnabled()){
+            tb_exprise_roman.setText("");
+        }
+        else{
+            tb_exprise_roman.setText(item.getRoman());
+        }
+
+        if(tb_exprise_hiragana.isEnabled()){
+            tb_exprise_hiragana.setText("");
+        }
+        else{
+            tb_exprise_hiragana.setText(item.getHiragana());
+        }
+
+        if(tb_exprise_katakana.isEnabled()){
+            tb_exprise_katakana.setText("");
+        }
+        else{
+            tb_exprise_katakana.setText(item.getKatakana());
+        }
+
+        tb_exprise_roman.setTag(item);
+        if(playVoice){
+            playVoice(item);
+        }
+    }
+
+    @SuppressLint("HandlerLeak")
+    private void initView() {
+        btnPlay = this.findViewById(R.id.btnPlay);
         tvPingjia = this.findViewById(R.id.tvPingjia);
         tvPianjia = this.findViewById(R.id.tvPianjia);
         tvRoman = this.findViewById(R.id.tvRoman);
 
-
         btnPlay.setBackgroundResource(R.mipmap.play);
         btnPlay.setOnClickListener(new View.OnClickListener(){
-            int i = 0;
             public void onClick(View v) {
-                if(!isPlay) {
-                    startTimer();
-                    btnPlay.setBackgroundResource(R.mipmap.pause);
-                }
-                else{
-                    stopTimer();
-                    btnPlay.setBackgroundResource(R.mipmap.play);
-                }
-                isPlay = !isPlay;
+                playClickHandler();
             }
         });
         final Button btnVoice = this.findViewById(R.id.btnVoice);
@@ -88,7 +290,7 @@ public class MainActivity extends AppCompatActivity {
                 int currentIndex = -1;
                 for(int i = 0;i<voiceItemList.size();i++)
                 {
-                    if(("罗马音："+voiceItemList.get(i).getRoman()).equals(tvRoman.getText()) )
+                    if(("罗马音："+voiceItemList.get(i).getRoman()).contentEquals(tvRoman.getText()) )
                     {
                         currentIndex = i;
                         break;
@@ -96,44 +298,81 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if(currentIndex >= 0)
                 {
-                    PlayVoice(voiceItemList.get(currentIndex));
+                    playVoice(voiceItemList.get(currentIndex));
                 }
             }
         });
-
         mHandler = new Handler(){
             @Override
             public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case Update_View:
-                        if(voiceIndex < 0){
-                            voiceIndex = 0;
+                if(msg != null){
+                    if (msg.what == Update_View) {
+                        if (viewVoiceIndex < 0) {
+                            viewVoiceIndex = 0;
                         }
-                        if(voiceIndex > voiceItemList.size() -1){
-                            voiceIndex = 0;
+                        if (viewVoiceIndex > voiceItemList.size() - 1) {
+                            viewVoiceIndex = 0;
                         }
-                        setContent(voiceItemList.get(voiceIndex));
-                        voiceIndex++;
-                        break;
-                    default:
-                        break;
+                        setViewContent(voiceItemList.get(viewVoiceIndex), true);
+                        viewVoiceIndex++;
+                    }
                 }
             }
         };
-
-        voiceItemList = loadXml("/storage/sdcard0/data/fifty_voice.raw");
-    }
-    private void setContent(VoiceItem item) {
-        tvPingjia.setText("平假名："+item.getHiragana());
-        tvPianjia.setText("片假名："+item.getKatakana());
-        tvRoman.setText("罗马音："+item.getRoman());
-        PlayVoice(item);
+        setViewContent(voiceItemList.get(viewVoiceIndex),false);
     }
 
-    private void PlayVoice(VoiceItem item) {
+    private void playClickHandler() {
+        if(!isPlay) {
+            startTimer();
+            btnPlay.setBackgroundResource(R.mipmap.pause);
+        }
+        else{
+            stopTimer();
+            btnPlay.setBackgroundResource(R.mipmap.play);
+        }
+        isPlay = !isPlay;
+    }
+
+    private void initTabs() {
+        TabHost tabHost = this.findViewById(android.R.id.tabhost);
+        tabHost.setup();
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.voice_view,tabHost.getTabContentView());
+        inflater.inflate(R.layout.voice_exprise,tabHost.getTabContentView());
+        tabHost.addTab(tabHost.newTabSpec("voice_view").setIndicator("学习").setContent(R.id.left));
+        tabHost.addTab(tabHost.newTabSpec("voice_exprise").setIndicator("练习").setContent(R.id.right));
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if(tabId.equals("voice_exprise")){
+                    if(isPlay){
+                        playClickHandler();
+                    }
+                }
+            }
+        });
+    }
+
+    private void setViewContent(VoiceItem item, boolean playVoice) {
+        if(item !=null){
+            tvPingjia.setText(String.format("平假名：%s", item.getHiragana()));
+            tvPianjia.setText(String.format("片假名：%s", item.getKatakana()));
+            tvRoman.setText(String.format("罗马音：%s", item.getRoman()));
+            if(playVoice){
+                playVoice(item);
+            }
+        }
+    }
+
+    private void playVoice(VoiceItem item) {
         try{
             if(item != null){
-                Uri uri = Uri.parse("android.resource://com.example.myapplication/raw/"+ item.getRoman());
+                String voiceId = item.getRoman();
+                if(voiceId.equals("do")){
+                    voiceId= voiceId+"_1";
+                }
+                Uri uri = Uri.parse("android.resource://com.example.myapplication/raw/"+ voiceId);
 
                 if(uri != null && tvRoman.getText().length() > 0)
                 {
@@ -148,13 +387,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private List<VoiceItem> loadXml(String file)
+    private List<VoiceItem> loadXml()
     {
-        List<VoiceItem> items = new ArrayList<VoiceItem>();
+        List<VoiceItem> items = new ArrayList<>();
         try {
-            File path = new File(file);
-
-            //FileInputStream fis = new FileInputStream(path);
             InputStream fis = this.getResources().openRawResource(R.raw.fifty_voice);
             // 获得pull解析器对象
             XmlPullParser parser = Xml.newPullParser();
@@ -163,10 +399,6 @@ public class MainActivity extends AppCompatActivity {
 
             int eventType = parser.getEventType(); // 获得事件类型
 
-            String id = null;
-            String name = null;
-            String gender = null;
-            String age = null;
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 String tagName = parser.getName(); // 获得当前节点的名称
                 VoiceItem item = new VoiceItem();
@@ -192,8 +424,6 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-
         }
         return items;
     }
@@ -212,9 +442,11 @@ public class MainActivity extends AppCompatActivity {
             };
         }
 
-        if(mTimer != null && mTimerTask != null )
+        int delay = 0;
+        int period = 2000;
+        if(mTimer != null){
             mTimer.schedule(mTimerTask, delay, period);
-
+        }
     }
 
     private void stopTimer(){
